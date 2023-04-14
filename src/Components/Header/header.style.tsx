0ecfa5f3082652c38d9332f51ee69ba8c512b6ac
William MIBELLI <william.mibelli@gmail.com>
2023-04-14 19:05:56 +0200

    style header

--- src/Components/Header/header.style.tsx
+++ src/Components/Header/header.style.tsx
@@ -3,13 +3,19 @@ import styled from "styled-components";
 export const Container = styled.div`
     width: 75%;
     margin: 0 auto;
+    padding-top: 10px;
     display: flex;
     flex-direction: row;
+    align-items: center;
+    border-top: 3px solid #7CA655;
+    border-right: 3px solid #7CA655;
+    border-radius: 4px 5px 4px 0
 
 `
 export const LinkContactContainer = styled.div`
     width: 200px;
     margin-left: auto;
+    margin-right: 10px;
     display: flex;
     align-items: flex-end;
     justify-content: flex-end;
@@ -18,6 +24,7 @@ export const LinkContactContainer = styled.div`
 export const Brand = styled.p`
     margin-left: 10px;
     margin-bottom: auto;
+    margin-top: auto;
     cursor: pointer;
     color: white;
     font-size: 3rem;
